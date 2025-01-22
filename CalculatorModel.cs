712ef2597bea6ba6016bfdff48faf4e03e43168c
Kevin Zhang <kevinzhang20031124@gmail.com>
2025-01-22 17:07:09 +0800

    add files (without bin, obj, Properties folder)

---
+++ CalculatorModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class CalculatorModel
+    {
+        private string _AddNum;
+
+        public string AddNum
+        {
+            get { return _AddNum; }
+            set
+            {
+                _AddNum = value;
+            }
+        }
+
+
+        private string _SubNum;
+
+        public string SubNum
+        {
+            get { return _SubNum; }
+            set
+            {
+                _SubNum = value;
+            }
+        }
+
+
+        private string _Result;
+
+        public string Result
+        {
+            get { return _Result; }
+            set
+            {
+                _Result = value;
+            }
+        }
+    }
+}
---
+++ CalculatorViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace calculator
+{
+    internal class CalculatorViewModel:INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private CalculatorModel _CalculatorM;
+
+        public CalculatorModel CalculatorM
+        {
+            get { 
+                if (_CalculatorM == null)
+                    _CalculatorM = new CalculatorModel();
+                return _CalculatorM; 
+            }
+            set { 
+                _CalculatorM = value;
+                RaisePropertyChanged("CalculatorM");
+            }
+        }
+
+
+        // 计算逻辑
+        void CalculateFunc ()
+        {
+            long result = 0;
+
+            long addnum = long.Parse(CalculatorM.AddNum);
+            long subnum = long.Parse(CalculatorM.SubNum);
+
+            object lockObject = new object();
+
+            Thread thread1 = new Thread(() => 
+            { 
+                lock (lockObject)
+                {
+                    result += addnum;
+                } 
+            });
+
+            Thread thread2 = new Thread(() => 
+            {
+                lock (lockObject) 
+                {
+                    result -= subnum;
+                }
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            CalculatorM.Result = result.ToString();
+
+            CalculatorM = CalculatorM;
+        }
+
+        // 清空逻辑
+        void ClearFunc()
+        {
+            CalculatorM.AddNum = "";
+            CalculatorM.SubNum = "";
+            CalculatorM.Result = "";
+
+            CalculatorM = CalculatorM;
+        }
+
+
+        bool CanCalculateExecute()
+        {
+            return true;
+        }
+
+        // 命令 绑定到计算按钮上
+        public ICommand CalculateAction
+        {
+            get
+            {
+                return new RelayCommand(CalculateFunc, CanCalculateExecute);
+            }
+        }
+
+        // 命令 绑定到清空按钮上
+        public ICommand ClearAction
+        {
+            get
+            {
+                return new RelayCommand(ClearFunc, CanCalculateExecute);
+            }
+        }
+
+    }
+}
